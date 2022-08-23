3911c09c5f52c0323f012707f7385d2dc1d3772f
dnknna <diko.kanafina@gmail.com>
2022-08-23 14:53:11 +0600

    init

---
+++ hv1.js
@@ -0,0 +1,165 @@
+04/08/2022 
+ 
+ //  hw1 
+ // const n3 = Math.random() * 10
+ // const n4 = Math.random() * 10
+ // const sym1 = "*"
+ // const sym2 = "="
+ // const total = n3*n4
+ // console.log(n3,sym1,n4,sym2 ,total)
+ 
+ //  hw2
+ // const n5 = Math.random() * 100 
+ // const word = "Рандомное число:"
+ // console.log(word,n5);
+ 
+ //  hw3
+ // console.log(Math.floor(Math.random() * 15) + 5);
+ 
+ //  hv4
+ // const name=prompt("Укажите своё имя:"),
+ // surname=prompt("Укажите свою фамилию:");
+ // alert(" Привет "+name+" "+surname);
+ 
+ 
+ 09/08/2022
+
+ //  hw1 
+ // const n1 = 1 
+ // const n2 = 2
+ // if (n2 > n1){
+ // console.log(n2);
+ // } else{
+ // console.log(n1);
+ // }
+ 
+ //  hw2 
+//  const a = 10;
+//  const b = 12;
+//  const c = 23;
+//  if (a > b && a > c) {
+//  console.log ('A');
+//  } else if (b > c && b > a)
+//   console.log ('B');
+//  else if (c > b && c > a)
+//  console.log ('C');
+//  else if (a == b && a == c)
+//  console.log  ('0');
+//  else if (a == b && b > c)
+//  console.log ("A,B");
+//  else if (a == c && c > b)
+//  console.log ("A,C");
+//  else if (b == c && c > a)
+//  console.log ("B,C");
+
+//  hw3
+// const un = "asdfg"
+// const pw = "asdfg52"
+// const nm = prompt('Username: ')
+// const word = prompt('Password: ')
+// console.log(un === nm , pw === word);
+// if(un === nm || pw === word) {
+//     console.log('Welcome Username', nm)
+// } else {
+//     console.log('Not found');
+// }
+
+11/08/2022
+
+//  hw1 +-
+// const n1 = Number(prompt("number1: "))
+// const s1 = prompt("sym1: ")
+// const n2 = Number(prompt("number2: "))
+// while (s1 === "+"){
+// console.log(n1 + n2);
+// }
+// else if(s1 === "-"){
+// console.log(n1 + n2);
+// }
+// else if(s1 === "/"){
+// console.log(n1 / n2);
+// }
+// else if(s1 === "*"){
+// console.log(n1 * n2);
+// }else{
+// console.log("Stop");
+// }
+
+
+//  hw2
+// var n1 = [1, 2, 3];
+// var m = Math.max(...n1);
+// console.log(m)
+
+
+//  hw3
+// var n = 5;
+// while (true) {
+//     var x = prompt("Введите число");
+//     if(isNaN(x)){
+//         alert("Ошибка ввода!");
+//     } else if (x<n){
+//         alert("Эх,мало!");
+//     } else if ( x>n){
+//         alert("Угого,много!");
+//     } else {
+//         alert ("Мои поздравления ты угадал число!");
+//         break;
+//     }
+// }
+
+//  hw4
+// const a = "Germany"
+// for( let i = 0; i < a.length; i++){
+//     console.log(a[i]);
+// }
+
+//  hw5
+// var i = 50;
+// while (i <= 100)
+// for (var i = 50; i <= 100; i++) {
+// 	console.log(i ++);
+// }
+
+//  hw6
+// var x = 9856;
+// var y = 0;
+// for(; x; x = Math.floor(x / 10)) {
+//     y *= 10;
+//     y += x % 10;
+// }
+//     console.log(y);
+
+
+// hw7 -
+
+16/08/2022
+//  hw1 -
+
+
+//  hw2 +-
+
+// var arr1 = [];
+// var arr2 = [];
+// var arr3 = [];
+// var arr4 = [];
+// var arr5 = [];
+// for ( i = 0; i < 1; i++ ) {
+// arr1.push( Math.round( Math.random() * 100 ));
+// arr2.push( Math.round( Math.random() * 100 ));
+// arr3.push( Math.round( Math.random() * 100 ));
+// arr4.push( Math.round( Math.random() * 100 ));
+// arr5.push( Math.round( Math.random() * 100 ));
+// 	}
+// console.log(arr1,arr2 ,arr3 ,arr4 ,arr5); 
+
+
+//  hw3
+// const numbers = [1,2,3,4,5,6,7,8,9];
+// const number = numbers.reduce((acc, number) => acc + number, 0);
+// console.log(number);
+
+18/08/2022
+
+
+
